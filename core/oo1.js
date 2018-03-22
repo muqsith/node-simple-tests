4319a89bdb5a6dc72ec94820843fd2781b767dc5
Muqsith Irfan <mui@one.com>
2018-03-22 11:48:19 +0400

    performance-wrapper.js

--- core/oo1.js
+++ core/oo1.js
@@ -1,8 +1,7 @@
 const crypto = require('crypto'),
-    util = require('util')
+    util = require('util'),
+    performanceWrapper = require('./performance-wrapper')
     ;
-
-
 class DatabaseConnector {
 
     constructor(a, b, c) {
@@ -45,80 +44,17 @@ class DatabaseConnector {
     }
 }
 
-
 class Dummy extends DatabaseConnector {
     constructor(a, b, c) {
         super(a, b, c)
     }
 }
 
+function logger({functionName, args, startTime, endTime}) {
+    console.log(functionName, util.inspect(args), startTime, endTime);
+}
 // creation of dal
-const _dal = new Dummy(1,2,3);
-
-function logger(fName, args, t1, t2) {
-    console.log(fName, util.inspect(args), t1, t2);
-}
-
-const getFunctionsNames = (obj) => {
-    // courtesy: https://stackoverflow.com/a/31055217/2388706
-    let fNames = [];
-    do {
-        fNames = fNames.concat(Object.getOwnPropertyNames(obj).filter((p) => (p !== 'constructor' && typeof obj[p] === 'function')));
-    } while ((obj = Object.getPrototypeOf(obj)) && obj !== Object.prototype);
-
-    return fNames;
-}
-
-const addTimeLogging = (obj) => {
-    let fNames = getFunctionsNames(obj);
-    for (let fName of fNames) {
-        let originalFunction = obj[fName];
-        let wrapperFunction = (...args) => {
-            let callbackFnIndex = -1;
-            let startTime = Date.now();
-            let _callBack = args.filter((arg, i) => {
-                    let _isFunction = (typeof arg === 'function');
-                    if (_isFunction) {
-                        callbackFnIndex = i;
-                    }
-                    return _isFunction;
-                })[0];
-            if (_callBack) {
-                let callbackWrapper = (...callbackArgs) => {
-                    let endTime = Date.now();
-                    logger(fName, args, startTime, endTime);
-                    _callBack.apply(null, callbackArgs);
-                }
-                args[callbackFnIndex] = callbackWrapper;
-            }
-            let originalReturnObject = originalFunction.apply(obj, args);
-            let isPromiseType = originalReturnObject && typeof originalReturnObject.then === 'function';
-            if (isPromiseType) {
-                originalReturnObject
-                .then((...resolveArgs) => {
-                    let endTime = Date.now();
-                    logger(fName, args, startTime, endTime);
-                    return Promise.resolve(resolveArgs);
-                })
-                .catch((...rejectArgs) => {
-                    let endTime = Date.now();
-                    logger(fName, args, startTime, endTime);
-                    return Promise.reject(rejectArgs);
-                })
-            }
-            if (!_callBack && !isPromiseType) {
-                let endTime = Date.now();
-                logger(fName, args, startTime, endTime);
-            }
-            return originalReturnObject;
-        }
-        obj[fName] = wrapperFunction;
-    }
-
-    return obj;
-}
-
-const dal = addTimeLogging(_dal);
+const dal = performanceWrapper(new Dummy(1,2,3), 'dal', logger);
 
 // users of dal
 console.log(dal.query0('muqsith'));
