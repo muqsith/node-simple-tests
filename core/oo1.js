7c4f7aa8ba18adbcc0616ef0b4b32377c5c49617
Muqsith Irfan <mui@one.com>
2018-03-22 12:12:28 +0400

    Created npm module out of it

--- core/oo1.js
+++ core/oo1.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto'),
     util = require('util'),
-    performanceWrapper = require('./performance-wrapper')
+    performanceWrapper = require('performance-wrapper')
     ;
 class DatabaseConnector {
 
