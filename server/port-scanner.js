6a6c55190cb139bf68ac26e769eaf3853e06bf97
muqsithirfan <abdulmuqsith.irfan@gmail.com>
2018-03-31 10:34:20 +0400

    Free port scanner

---
+++ server/port-scanner.js
@@ -0,0 +1,20 @@
+const findPort = require('find-port');
+
+const getFreePort = (range) => {
+    return (
+        new Promise((resolve, reject) => {
+            findPort('127.0.0.1', range.min, range.max, (ports) => {
+                let port = ports[((Math.random() * ports.length) | 0)];
+                resolve(port);
+            })
+        })
+    );
+}
+
+getFreePort({min: 35000, max:45000})
+.then((port) => {
+    console.log(port);
+})
+.catch((err) => {
+    console.log(err);
+})
