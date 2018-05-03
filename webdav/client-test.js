8dd47f4ad4993b4f95f8b25423665b2d725afba1
Muqsith Irfan <mui@one.com>
2018-05-03 12:26:17 +0400

    WebDAV client

---
+++ webdav/client-test.js
@@ -0,0 +1,6 @@
+const client = require('webdav')('http://localhost/');
+client
+    .getDirectoryContents("/")
+    .then(function(contents) {
+        console.log(JSON.stringify(contents, undefined, 4));
+    });
