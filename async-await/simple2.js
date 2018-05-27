7517a4cdd2d49306f3eae155fcf99373104fb9e2
Muqsith Irfan <mui@one.com>
2018-05-27 14:26:02 +0400

    More testes

---
+++ async-await/simple2.js
@@ -0,0 +1,16 @@
+function moody () {
+    return (
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log(`${new Date()} :: Hello`);
+                resolve();
+            }, 1000);
+        })
+    )
+};
+
+async function runMoody() {
+    await moody();
+}
+
+runMoody();
---
+++ lodash/memoization.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+const uuidv4 = require('uuid/v4');
+const 
+
+let ids = [];
+
+for (let i = 0; i < 1000; i += 1) {
+    ids.push(uuidv4());
+}
+
+const get
