deb79f7392477a62edc2406692a72b7395daf7a8
Muqsith Irfan <mui@one.com>
2018-12-12 10:35:30 +0400

    read method learning done

--- streams/readline-stream.js
+++ streams/readline-stream.js
@@ -2,7 +2,10 @@ const path = require('path');
 const fs = require('fs');
 
 
-const dataFile = '/Users/abdulmuqsith/Development/data/942312891_T_ONTIME_REPORTING.csv';
+//const dataFile = '/Users/abdulmuqsith/Development/data/942312891_T_ONTIME_REPORTING.csv';
+const dataFile = '/home/mui/Development/data/orders.json';
+
+const outFile = '/home/mui/Development/data/orders_out.json';
 
 const getFd = (filePath) => {
     const promiseHandler = (resolve, reject) => {
@@ -30,20 +33,53 @@ const getFstat = (fd) => {
     return new Promise(promiseHandler);
 }
 
-async function _main() {
+
+const getFileReader = async (filePath) => {
     const fd = await getFd(dataFile);
     const stat = await getFstat(fd);
     const fileSize = stat.size;
-    const chunkSize = 16 * 1024;
-    const buf = Buffer.allocUnsafe(chunkSize);
+    let chunkSize = 16 * 1024;
     let position = 0;
-    fs.read(fd, buf, 0, chunkSize, position, (err, bytesRead, buffer) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(buffer.toString('utf8'));
-        }
-    });
+
+    return reader = () => {
+        return new Promise((resolve, reject) => {
+            if ((fileSize - position) < chunkSize) {
+                chunkSize = fileSize - position;
+            }
+            fs.read(fd, Buffer.alloc(chunkSize), 0, chunkSize, position, (err, bytesRead, buffer) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    if (position < fileSize) {
+                        resolve(buffer.toString('utf8'));
+                    } else {
+                        resolve(null);
+                    }
+                    position += bytesRead;
+                }
+            });
+        })
+    };
+}
+
+async function _main() {
+    const reader = await getFileReader(dataFile);
+    let data = null;
+
+    // data = await reader();
+    // console.log(data);
+    // data = await reader();
+    // console.log(data);
+
+    const writeStream = fs.createWriteStream(outFile);
+
+    while ((data = await reader())) {
+        writeStream.write(data);
+    }
+    writeStream.end();
+
+
+
 }
 
 _main();
