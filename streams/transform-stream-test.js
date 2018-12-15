fac17dec02c4600185b9d4c90ea4e349fbddb9d3
Muqsith Irfan <mui@one.com>
2018-12-15 09:56:51 +0400

    Learned a little about transform strams

---
+++ streams/transform-stream-test.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+const { Transform, PassThrough } = require('stream');
+
+const inputFile = path.resolve('/home/mui/Development/data/products.json');
+const outputFile = path.resolve('/home/mui/Development/data/products-out.json');
+
+// PassThrough is a stream that does nothing to the data but just allows datat to pass though it
+
+
+class PurifyJson extends Transform {
+    constructor(options) {
+        super(options);
+    }
+
+    _transform(chunk, encoding, callback) {
+        const str = chunk.toString('utf8');
+        callback(null, JSON.stringify(str.length) + '\n');
+    }
+}
+
+
+
+async function test() {
+    const inputStream = fs.createReadStream(inputFile);
+    const outputStream = fs.createWriteStream(outputFile);
+    const purifyJsonStream = new PurifyJson();
+    inputStream.pipe(purifyJsonStream).pipe(outputStream);
+}
+
+test();
