52eb93810d15b9633d6511e2e3928b23bc728702
Muqsith Irfan <mui@one.com>
2018-12-15 16:45:15 +0400

    Winston log error stack trace

--- loggers/winston-test.js
+++ loggers/winston-test.js
@@ -5,12 +5,22 @@ const winston = require('winston');
 
 const filename = path.resolve(__dirname, '..', 'logs', 'winston-test.log');
 
+const errorStackFormat = winston.format(info => {
+    if (info instanceof Error) {
+      return Object.assign({}, info, {
+        stack: info.stack,
+        message: info.message
+      })
+    }
+    return info
+  })
 
 const logger = winston.createLogger({
     transports: [
         new winston.transports.File({ filename })
-    ]
+    ],
+    format: winston.format.combine(errorStackFormat(), winston.format.json())
 });
 
 logger.info('your are really beautiful');
-logger.error('shut the Fuck up');
+logger.error(new Error('muqsith'));
