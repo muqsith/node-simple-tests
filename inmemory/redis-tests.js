760d14b8c4e82125ebc149068e0b2fad0c61782d
Muqsith Irfan <mui@one.com>
2018-12-17 09:47:16 +0400

    Redis tests

--- inmemory/redis-tests.js
+++ inmemory/redis-tests.js
@@ -1,48 +1,79 @@
 const path = require('path');
-const { Writable } = require('stream');
+const fs = require('fs-extra');
+const { Writable, Transform } = require('stream');
 const redis = require('redis');
 const getReadLineStream = require('streaming-line-reader');
 
 
 
 const inputFile = path.resolve('/home/mui/Development/data/enwiktionary.json');
+const keyFile = path.resolve('/home/mui/Development/data/enwiktionary.keys');
+const outFile = path.resolve('/home/mui/Development/data/enwiktionary-redis-out.json');
 
 class RedisWriter extends Writable {
     constructor(redisClient, options) {
         super(options);
-        this.cachedObjectLifeTime = 600; // value in seconds (600 = 10 minutes)
         this.redisClient = redisClient;
-        this.failed = 0;
-        this.insertedCount = 0;
     }
 
     _write(chunk, encoding, callback) {
         const str = chunk.toString('utf8');
         try {
             const o = JSON.parse(str);
-            this.redisClient.set(o.id, o.data, (err) => {
+            const key = o.id;
+            this.redisClient.set(key, o.data, (err) => {
                 if (err) {
                     callback(err);
                 } else {
-                    this.insertedCount += 1;
-                    callback();
+                    fs.writeFile(keyFile, key.trim() + '\n', {flag: 'a'}, (err) => {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback();
+                        }
+                    });
                 }
             });
         } catch (e) {
-            this.failed += 1;
-            console.log(`Failed ${this.failed}`);
-            callback();
+            callback(e);
         }
+    }
+}
+
+class RedisReader extends Transform {
+    constructor(redisClient, options) {
+        super(options);
+        this.redisClient = redisClient;
+    }
+
+    _transform(chunk, encoding, callback) {
+        const key = chunk.toString('utf8').trim();
+        this.redisClient.get(key, (err, data) => {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, data.trim() + '\n');
+            }
+        });
     }
 }
 
 async function _main() {
     const lineStream = await getReadLineStream(inputFile);
     const redisClient = redis.createClient();
-    const redisWriter = new RedisWriter(redisClient);
+    await fs.remove(keyFile);
+    const redisWriter = new RedisWriter(redisClient, keyFile);
     lineStream.pipe(redisWriter);
-    redisWriter.on('finish', () => {
-        redisClient.end();
+    redisWriter.on('finish', async () => {
+        const keyLineStream = await getReadLineStream(keyFile);
+        const outFileStream = fs.createWriteStream(outFile);
+        const transformStream = new RedisReader(redisClient);
+        keyLineStream.pipe(transformStream).pipe(outFileStream);
+        outFileStream.on('finish', () => {
+            redisClient.quit(() => {
+                console.log('Redis flushed and connection closed');
+            });
+        });
     });
 }
 
