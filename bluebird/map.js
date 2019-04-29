db88a2217c2822fe5cf4d4902a8f5aba2b5cdce5
Muqsith Irfan <mui@one.com>
2019-04-29 11:11:29 +0400

    bluebird Promise.map test

---
+++ bluebird/map.js
@@ -0,0 +1,24 @@
+const Promise = require('bluebird');
+
+const promises = [
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(20)
+        }, Math.random() * 10000);
+    }),
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(30)
+        }, Math.random() * 1000);
+    }),
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(40)
+        }, Math.random() * 1000);
+    })
+];
+
+Promise.map(promises, (p) => p)
+.then((result) => {
+    console.log(result);
+});
