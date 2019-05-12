8d390c165e16d1dcf5f3dbf8d272f6b6417107b6
Muqsith Irfan <mui@one.com>
2019-05-12 12:56:43 +0400

    Added async await tests

---
+++ promises/asaw.js
@@ -0,0 +1,23 @@
+function test1() {
+    return Promise.resolve(10);
+}
+
+async function test2() {
+    return 20;
+}
+
+async function main () {
+    const v1 = await test1();
+    const v2 = await test2();
+    console.log(v1, v2);
+}
+
+main();
+
+
+function main2() {
+    test1().then((v1) => console.log(v1));
+    test2().then((v2) => console.log(v2));
+}
+
+main2();
