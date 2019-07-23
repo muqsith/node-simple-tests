d2c700b6407b3d7ca3eb8e7ff377bab13e750ef2
Muqsith Irfan <mui@one.com>
2019-07-23 10:35:53 +0400

    static methods test in es6

---
+++ es6/staticMethodTests.js
@@ -0,0 +1,23 @@
+class Employee {
+    constructor(details) {
+        this.firstName = details.firstName;
+        this.lastName = details.lastName;
+    }
+
+    getFullName() {
+        return this.firstName +  ' ' + this.lastName;
+    }
+
+    static getNameLength(name) {
+        return (typeof name === 'string') ? name.length : 0;
+    }
+
+    printDetails() {
+        console.log(this.getFullName());
+        console.log('Name length: ', Employee.getNameLength(this.getFullName()));
+    }
+}
+
+const emp = new Employee({firstName: 'Mohammed', lastName: 'Irfan'});
+
+emp.printDetails();
