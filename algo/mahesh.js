4b1125496393b7b399b2745762491b6d7d616c1e
Muqsith Irfan <mui@one.com>
2019-08-14 16:59:48 +0400

    sorting arrays by Mahesh

---
+++ algo/mahesh.js
@@ -0,0 +1,22 @@
+function getResult(input, position) {
+    const r = new Array(position);
+    input.forEach(sa => {
+        for (let i = 0; i < r.length; i++) {
+            if (sa[i] >= r[position - 1]) {
+                break;
+            }
+            r.push(sa[i]);
+            r.sort((a, b) => a - b).pop();
+        }
+    })
+    return r.pop();
+}
+
+const sample2 = [
+    [1, 5, 5, 8],
+    [2, 6, 6, 9, 13],
+    [3, 4, 4, 7, 20]
+];
+
+
+console.log(getResult(sample2, 1));
