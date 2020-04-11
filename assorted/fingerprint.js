94cca788bc7e07c2d52adfdf2774b7925e3cd507
mui <mui@one.com>
2020-04-11 13:25:04 +0400

    tried fingerprint2.js

---
+++ assorted/fingerprint.js
@@ -0,0 +1,7 @@
+const Fingerprint2  = require('fingerprintjs2');
+
+setTimeout(function () {
+    Fingerprint2.get(function (components) {
+      console.log(components) // an array of components: {key: ..., value: ...}
+    })
+}, 500);
---
+++ networking/dns.js
@@ -0,0 +1,9 @@
+const dns = require('dns');
+
+dns.resolveCname('devdxb1.1prod.one', (err, result) => {
+    if (err) {
+        console.error('Error ==========>>>>>>>>>>>', err);
+    } else {
+        console.log(result);
+    }
+});
