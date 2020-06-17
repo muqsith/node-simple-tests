6ca5af4ab38db81fe3f43bc192ac8fb74bf6d95e
mui <mui@one.com>
2020-06-17 11:24:43 +0400

    mock-fs tests

--- fs/mockFsTest.js
+++ fs/mockFsTest.js
@@ -13,15 +13,21 @@ const mock = require('mock-fs');
 //   '/tmp/to/other/path': {/** another empty directory */}
 // });
 
-mock();
-
 async function test() {
+  mock();
   const dir = path.resolve('trmp', 'to', 'fake', 'dir');
   await fs.ensureDir(dir);
   const filePath = path.resolve(dir, 'hello.txt');
   await fs.writeFile(filePath, 'Hello World');
   const data = await fs.readFile(filePath);
   console.log(data.toString('utf-8'));
+  mock.restore();
+
+  // real data
+  const sampleFilePath = path.resolve(__dirname, '..', 'data', 'sample.txt');
+  console.log(sampleFilePath);
+  const sampleData = await fs.readFile(sampleFilePath);
+  console.log(sampleData.toString('utf-8'));
 }
 
 
