7e34f70e280d1613ebc34c91c0522b8515bc3f08
mui <mui@one.com>
2021-12-25 13:55:31 +0400

    Learned about middleware routing

---
+++ server/express/mw/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mw1 = require('./mw1');
+const mw2 = require('./mw2');
+
+const router = express.Router();
+
+app.use((req, res, next) => {
+    console.log('====> Request ...');
+    next();
+    console.log('<==== Response ...');
+})
+// .use('/abc', router.use((req, res, next) => {
+//     res.send('hello world');
+// }))
+.use('/abc', router.use(mw1).use(mw2))
+.listen(4545, (err) => {
+    if (err) {
+        console.error('Error occured: ', err);
+    } else {
+        console.log('Listening : http://localhost:4545/');
+    }
+});
---
+++ server/express/mw/mw2.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router
+.use((req, res, next) => {
+  console.log('i am in middleware two');
+  next();
+})
+.get('', (req, res, next) => {
+  res.send('get 1 - from middleware 2');
+});
+
+module.exports = router;
