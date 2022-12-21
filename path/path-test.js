8ad108634bf3c78abe8b0e37d610923e01988962
muqsith <muqsithirfan@gmail.com>
2022-12-21 07:53:51 +0530

    Learned more about filepaths

---
+++ path/path-test.js
@@ -0,0 +1,17 @@
+const pathModule = require("path");
+
+console.log(__dirname);
+console.log(process.cwd());
+
+const pathArg = process.argv[2];
+
+console.log(
+  `path ${pathArg} is ${
+    pathModule.isAbsolute(pathArg) ? "absolute path" : "relative path"
+  }`
+);
+
+if (!pathModule.isAbsolute(pathArg)) {
+  const absolutePath = pathModule.resolve(process.cwd(), pathArg);
+  console.log("absolutePath = ", absolutePath);
+}
