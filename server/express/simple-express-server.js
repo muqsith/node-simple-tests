311ca09f36f3205c38d2f318feb9cd9e044bcd8f
mui <mui@one.com>
2023-04-02 14:52:03 +0400

    cookie test

--- server/express/simple-express-server.js
+++ server/express/simple-express-server.js
@@ -1,7 +1,19 @@
 const { getCircularReplacer } = require("../../utils");
-
 const express = require("express");
+
+const cors = require("cors");
+
 const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders:
+      "Origin,Content-Type,Content-Length,Authorization,Accept,X-Requested-With",
+  })
+);
 
 app.use(express.json());
 
@@ -20,6 +32,35 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log("request payload", req.body);
   return res.status(200).send({ status: "ok" });
+});
+
+app.get("/ping", (req, res) => {
+  return res.status(200).send("ok");
+});
+
+app.get("/login", (req, res) => {
+  console.log(">>>>>>>>>>>>>>>>>>>> req.cookies:", req.headers.cookie);
+  return res.status(200).send("ok");
+});
+
+app.get("/login-check", (req, res) => {
+  console.log(">>>>>>>>>>>>>>>>>>>> req.cookies:", req.headers.cookie);
+  return res.status(200).send("ok");
+});
+
+app.get("/get-cookie", (req, res) => {
+  res.cookie(
+    "muqsith-test-cookie",
+    `Muqsith's test cookie - set on - ${new Date()}`,
+    {
+      maxAge: 9000000000,
+      domain: "11219b2f97d3.ngrok.io",
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    }
+  );
+  return res.status(200).send("ok");
 });
 
 app.listen(4545, (err) => {
